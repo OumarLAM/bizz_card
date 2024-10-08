29d659caf8ff48b148fba5579593dfc63563d550
OumarLAM <oumarlam154@gmail.com>
2024-10-08 23:58:10 +0000

    Add card informations

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class AppBarExample extends StatelessWidget {
         actions: <Widget>[
           IconButton(
             icon: const Icon(Icons.qr_code_2),
-            tooltip: 'qr code',
+            tooltip: 'QR code',
             onPressed: () {},
           ),
         ],
@@ -43,11 +43,73 @@ class AppBarExample extends StatelessWidget {
       body: Center(
         child: Container(
           decoration: const BoxDecoration(
-          color: Color.fromARGB(255, 245, 245, 245),
+            color: Color.fromARGB(255, 245, 245, 245),
             borderRadius: BorderRadius.only(
               topLeft: Radius.circular(10),
               topRight: Radius.circular(10),
             ),
+          ),
+          child: const Stack(
+            children: [
+              Positioned(
+                top: 150,
+                left: 150,
+                child: CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('assets/profile.jpeg'),
+                ),
+              ),
+              Positioned(
+                top: 260,
+                left: 125,
+                child: Text(
+                  'Oumar LAM',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 300,
+                left: 150,
+                child: SizedBox(
+                  width: 200,
+                  child: Text(
+                    '24 years old',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 400,
+                left: 110,
+                child: SizedBox(
+                  width: 200,
+                  child: Text(
+                    'oumarlamsn@gmail.com',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 460,
+                left: 125,
+                child: SizedBox(
+                  width: 200,
+                  child: Text(
+                    '+221 77 346 35 12',
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              )
+            ],
           ),
         ),
       ),
